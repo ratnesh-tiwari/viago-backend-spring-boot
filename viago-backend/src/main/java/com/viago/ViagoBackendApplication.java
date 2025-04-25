eb566cdbb5ac384a47ccef5705b26a8bb834cc50
Ratnesh Kumar Tiwari <t.ratnesh155@gmail.com>
2025-04-25 19:44:16 +0530

    initial commit

---
+++ viago-backend/src/main/java/com/viago/ViagoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.viago;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ViagoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ViagoBackendApplication.class, args);
+	}
+
+}
---
+++ viago-backend/src/test/java/com/viago/ViagoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.viago;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ViagoBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
